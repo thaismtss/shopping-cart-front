b0555462e5d7c948049ffaaa7d8e28d2ed207016
Thais Martins <thaismartins1999@gmail.com>
2024-04-16 07:40:22 -0300

    feat: create product skeleton component

---
+++ app/Components/ProductSkeleton/index.tsx
@@ -0,0 +1,11 @@
+import { Skeleton, Stack } from "@chakra-ui/react";
+
+export default function ProductSkeleton() {
+  return (
+    <Stack maxW="sm" flexBasis="calc(33.3333% - 20px)" margin="10px">
+      <Skeleton height="200px" />
+      <Skeleton height="20px" />
+      <Skeleton height="20px" />
+    </Stack>
+  );
+}
